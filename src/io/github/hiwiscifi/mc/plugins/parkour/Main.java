509c486f2f08c3ba5576e61950b9302ce5301cc6
HiWiSciFi <max.j.hager@gmail.com>
2020-11-21 19:25:54 +0100

    basic enable and disable functions added

--- src/io/github/hiwiscifi/mc/plugins/parkour/Main.java
+++ src/io/github/hiwiscifi/mc/plugins/parkour/Main.java
@@ -6,11 +6,16 @@ public class Main extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		
+		System.out.println("Initializing parkour plugin...");
+		System.out.println("Parkour plugin initialized!");
 	}
 	
 	@Override
 	public void onDisable() {
+		System.out.println("Preventing memory leaks...");
+		System.out.println("done");
 		
+		System.out.println("Saving data to files...");
+		System.out.println("done");
 	}
 }
