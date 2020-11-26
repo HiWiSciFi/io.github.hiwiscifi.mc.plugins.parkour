662bfb9a2eeca8808d0af18d11be627b07b98a7f
imurnaili <54451121+imurnaili@users.noreply.github.com>
2020-11-26 09:06:53 +0100

    itemUse gets data now

--- src/io/github/hiwiscifi/mc/plugins/parkour/listeners/Listener_ItemUse.java
+++ src/io/github/hiwiscifi/mc/plugins/parkour/listeners/Listener_ItemUse.java
@@ -3,10 +3,18 @@ package io.github.hiwiscifi.mc.plugins.parkour.listeners;
 
 
 
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
+
+import io.github.hiwiscifi.mc.plugins.parkour.Main;
 
 public class Listener_ItemUse implements Listener{
 
@@ -23,10 +31,38 @@ public class Listener_ItemUse implements Listener{
 	@EventHandler
 	public void onPlayerUse(PlayerInteractEvent event)
 	{
-
 		Player p = event.getPlayer();
-
-		System.out.println(p.getInventory().getItemInMainHand());
+		
+		ItemStack item = p.getInventory().getItemInMainHand();
+		
+		Material mat = item.getType();
+		
+		if(mat.isAir()) {
+			return;
+		}
+		
+		ItemMeta itemMeta = item.getItemMeta();
+		
+		PersistentDataContainer itemData = itemMeta.getPersistentDataContainer();
+		
+		NamespacedKey key = new NamespacedKey(Main.getInstance(),"parkour_func");
+		itemData.has(key, PersistentDataType.STRING);
+		
+		String func = itemData.get(key, PersistentDataType.STRING);
+		
+		switch(func) {
+			case "resetCheckpoint":
+				
+				break;
+				
+			case "resetStart":
+				
+				break;
+				
+			case "resetSpawn":
+				
+				break;				
+		}
 
 	}
 }
