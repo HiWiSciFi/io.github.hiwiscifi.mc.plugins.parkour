28e64735174b6d0f973c3482210191bfe0a61788
HiWiSciFi <max.j.hager@gmail.com>
2020-11-26 12:14:11 +0100

    Update ParkourCheckpoint.java

--- src/io/github/hiwiscifi/mc/plugins/parkour/ParkourCheckpoint.java
+++ src/io/github/hiwiscifi/mc/plugins/parkour/ParkourCheckpoint.java
@@ -1,5 +1,12 @@
 package io.github.hiwiscifi.mc.plugins.parkour;
 
+import org.bukkit.Location;
+
 public class ParkourCheckpoint {
 	
+	public Location location;
+	
+	public ParkourCheckpoint(Location location) {
+		this.location = location;
+	}
 }
