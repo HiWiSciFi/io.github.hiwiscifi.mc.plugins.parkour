d632877a33b49bf85c8f4453564dda7bbe9b0314
Scarf <roman.kress1@gmail.com>
2020-12-01 00:18:23 +0100

    Teleporteffect added through haxs torment
    Hax-sensei is my hero

--- src/io/github/hiwiscifi/mc/plugins/parkour/utils/effects/TeleportEffect.java
+++ src/io/github/hiwiscifi/mc/plugins/parkour/utils/effects/TeleportEffect.java
@@ -2,19 +2,24 @@ package io.github.hiwiscifi.mc.plugins.parkour.utils.effects;
 
 import java.util.Map;
 
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
 import io.github.hiwiscifi.mc.plugins.parkour.utils.ParkourEffect;
 
 public class TeleportEffect extends ParkourEffect{
 
 	public TeleportEffect(String[] args) {
 		super(args);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
-	public void apply() {
-		// TODO Auto-generated method stub
-
+	public void apply(Player player) {
+		double x = Double.parseDouble(arguments[1]);
+		double y = Double.parseDouble(arguments[2]);
+		double z = Double.parseDouble(arguments[3]);
+		Location place = new Location(player.getWorld(), x, y, z);
+		player.teleport(place);
 	}
 
 	@Override
