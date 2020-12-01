761b1e6665016cc3601d47290d23bc478cab287f
HiWiSciFi <max.j.hager@gmail.com>
2020-12-01 12:05:05 +0100

    testing

--- src/io/github/hiwiscifi/mc/plugins/parkour/Main.java
+++ src/io/github/hiwiscifi/mc/plugins/parkour/Main.java
@@ -39,9 +39,8 @@ public class Main extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		instance = this;
-
+		
 		US.initializeKeys();
-
 		System.out.println(US.fillWithMinus("Initializing Parkour plugin", 40));
 
 		System.out.println(US.OUT_PREFIX + "Registring commands" + US.THREE_DOTS);
