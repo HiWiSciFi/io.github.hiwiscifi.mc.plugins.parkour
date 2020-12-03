d6c8d84dbf156ddaa8982182a021839eded425fc
Scarf <roman.kress1@gmail.com>
2020-12-03 19:29:24 +0100

    relatives Teleportieren & Potioneffekte
    relatives Teleportieren eingefügt; Location ist nun globale Variable;
    Potioneffekte sind nun appliybar;

--- src/io/github/hiwiscifi/mc/plugins/parkour/utils/effects/EffectEffect.java
+++ src/io/github/hiwiscifi/mc/plugins/parkour/utils/effects/EffectEffect.java
@@ -1,20 +1,37 @@
 package io.github.hiwiscifi.mc.plugins.parkour.utils.effects;
 
 import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import io.github.hiwiscifi.mc.plugins.parkour.utils.ParkourEffect;
 
 public class EffectEffect extends ParkourEffect{
 
+	PotionEffect effect;
 	public EffectEffect(String[] args) {
+
 		super(args);
-		// TODO Auto-generated constructor stub
+
+		String type = arguments[1];
+		int amplifier = Integer.parseInt(arguments[2]);
+		int length = 999999;
+
+		if(arguments.length >= 4) {
+			length = Integer.parseInt(arguments[3]);
+		}
+
+		effect = new PotionEffect(
+				PotionEffectType.getByName(type),
+				amplifier,
+				length,
+				false,
+				false);
 	}
 
 	@Override
 	public void apply(Player player) {
-		// TODO Auto-generated method stub
-
+		player.addPotionEffect(effect);
 	}
 
 
--- src/io/github/hiwiscifi/mc/plugins/parkour/utils/effects/TeleportEffect.java
+++ src/io/github/hiwiscifi/mc/plugins/parkour/utils/effects/TeleportEffect.java
@@ -7,17 +7,36 @@ import io.github.hiwiscifi.mc.plugins.parkour.utils.ParkourEffect;
 
 public class TeleportEffect extends ParkourEffect{
 
+	private double x,y,z;
+	private boolean relativ = false;
+
 	public TeleportEffect(String[] args) {
+
 		super(args);
+
+		x = Double.parseDouble(arguments[1]);
+		y = Double.parseDouble(arguments[2]);
+		z = Double.parseDouble(arguments[3]);
+
+		if (arguments.length >= 5) {
+			relativ = "true" == arguments[4];
+		}
 	}
 
 	@Override
 	public void apply(Player player) {
-		double x = Double.parseDouble(arguments[1]);
-		double y = Double.parseDouble(arguments[2]);
-		double z = Double.parseDouble(arguments[3]);
-		Location place = new Location(player.getWorld(), x, y, z);
-		player.teleport(place);
+
+		if (!relativ) {
+			Location place = new Location(player.getWorld(), x, y, z);
+			player.teleport(place);
+		}
+		else {
+			Location playerPlace = player.getLocation();
+			playerPlace.add(x, y, z);
+			player.teleport(playerPlace);
+		}
 	}
+
+
 
 }
