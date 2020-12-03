d62d77f131e8072ad4d105d1c74c2ca33f592fe9
imurnaili <54451121+imurnaili@users.noreply.github.com>
2020-12-03 14:51:36 +0100

    restart parkour

--- src/io/github/hiwiscifi/mc/plugins/parkour/listeners/Listener_PressurePlate.java
+++ src/io/github/hiwiscifi/mc/plugins/parkour/listeners/Listener_PressurePlate.java
@@ -94,37 +94,39 @@ public class Listener_PressurePlate implements Listener {
 					return;
 				}
 				ParkourHelper.startCheckpoint(player, parkour, currentCheckpoint + 1);
-			}else if (currentCheckpoint > 0 && parkour.checkpoints.get(currentCheckpoint-1).location.distance(ablock.getLocation()) < 0.25d) {
-				ParkourHelper.restartCheckpoint(player, parkour, currentCheckpoint);
+			}else if (currentCheckpoint > 0) {
+				if (parkour.checkpoints.get(currentCheckpoint-1).location.distance(ablock.getLocation()) < 0.25d) {
+					ParkourHelper.restartCheckpoint(player, parkour, currentCheckpoint);
 
-			} else {
-				if(currentCheckpoint > 1) {
-					for (int i = 0; i < parkour.checkpoints.get(currentCheckpoint-1).effectPoints.size(); i++) {
-						if (parkour.checkpoints.get(currentCheckpoint-1).effectPoints.get(i).location.distance(ablock.getLocation()) < 0.25d) {
-							ParkourHelper.applyEffect(player, parkour.checkpoints.get(currentCheckpoint-1).effectPoints.get(i));
+				} else {
+					if(currentCheckpoint > 1) {
+						for (int i = 0; i < parkour.checkpoints.get(currentCheckpoint-1).effectPoints.size(); i++) {
+							if (parkour.checkpoints.get(currentCheckpoint-1).effectPoints.get(i).location.distance(ablock.getLocation()) < 0.25d) {
+								ParkourHelper.applyEffect(player, parkour.checkpoints.get(currentCheckpoint-1).effectPoints.get(i));
+							}
 						}
 					}
-				}
-				else {
-					for (int i = 0; i < parkour.startCheckpoint.effectPoints.size(); i++) {
-						if (parkour.startCheckpoint.effectPoints.get(i).location.distance(ablock.getLocation()) < 0.25d) {
-							ParkourHelper.applyEffect(player, parkour.startCheckpoint.effectPoints.get(i));
+					else {
+						for (int i = 0; i < parkour.startCheckpoint.effectPoints.size(); i++) {
+							if (parkour.startCheckpoint.effectPoints.get(i).location.distance(ablock.getLocation()) < 0.25d) {
+								ParkourHelper.applyEffect(player, parkour.startCheckpoint.effectPoints.get(i));
+							}
 						}
 					}
 				}
 			}
+			else if(currentCheckpoint == 0 && parkour.startCheckpoint.location.distance(ablock.getLocation()) < 0.25d) {
+				ParkourHelper.restartParkour(player, parkour);
+			}
+
+			ParkourHelper.restartParkour(player, parkour);
 
 
 		} else {
 			for (Parkour parkour : Main.getInstance().parkours) {
 				if (parkour.startCheckpoint != null) {
 					if (parkour.startCheckpoint.location.distance(ablock.getLocation()) < 0.25d) {
-						if(!onParkour) {
 							ParkourHelper.startParkour(player, parkour);
-						}
-						else {
-							ParkourHelper.restartParkour(player, parkour);
-						}
 					}
 				}
 			}
