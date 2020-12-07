3a7c6c9170eedfc11ec388773acaec70136469c5
HiWiSciFi <max.j.hager@gmail.com>
2020-12-07 11:44:59 +0100

    Update ParkourItems.java

--- src/io/github/hiwiscifi/mc/plugins/parkour/utils/ParkourItems.java
+++ src/io/github/hiwiscifi/mc/plugins/parkour/utils/ParkourItems.java
@@ -37,8 +37,6 @@ public class ParkourItems {
 			return;
 		}
 		
-		int currentCheckpoint = player.getPersistentDataContainer().get(US.currentCheckpointKey, PersistentDataType.INTEGER);
-		
 		switch(itemConfigString) {
 		case US.itemVal_reset_checkpoint:
 			player.setHealth(0d);
